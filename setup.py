23fae313b1353df34e574db433321a157786b4c8
smurphy <steven.t.murphy3@gmail.com>
2023-05-06 16:17:15 -0400

    Updating requirements

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from distutils.core import setup
 
 setup(
-      # name='Basic Chemistry Tools',
-      # version='0.0',
-      # description='Provides Basic Chemistry Functionality',
-      # author='Steven Murphy',
-      # author_email='steven.t.murphy3@gmail.com',
-      # url='https://github.com/bigboss3333/basic_chem',
-      # packages=['core_chem'],
-     )
+    # name='Basic Chemistry Tools',
+    # version='0.0',
+    # description='Provides Basic Chemistry Functionality',
+    # author='Steven Murphy',
+    # author_email='steven.t.murphy3@gmail.com',
+    # url='https://github.com/bigboss3333/basic_chem',
+    # packages=['core_chem'],
+)
